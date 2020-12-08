53eb96cca4b2d1251294f53f7c369585855d4b0a
Paul Lether <paul.lether@cloudsolutions.co.uk>
2020-12-08 00:14:39 +0000

    completed day two

---
+++ 2/password_philosophy.go
@@ -0,0 +1,45 @@
+package main
+import (
+    "bufio"
+    "log"
+	"os"
+	"regexp"
+	"strings"
+	"strconv"
+	"fmt"
+)
+
+func main() {
+	// reading file
+	file, err := os.Open("input.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+	scanner := bufio.NewScanner(file)
+	r:= regexp.MustCompile(`^(\d+)-(\d+) ([a-z]): ([a-z]+)$`)
+
+	valid := 0
+	partTwoValid := 0
+    for scanner.Scan() {
+		// part one 
+		matches := r.FindStringSubmatch(scanner.Text())
+		min, _ := strconv.Atoi(matches[1])
+		max, _ := strconv.Atoi(matches[2])
+		letter := matches[3]
+		password := matches[4]
+
+		if  min <= strings.Count(password, letter) && strings.Count(password, letter) <= max{
+			valid++
+		}
+
+		// part two
+		if (max <= len(password) && string(password[min -1]) == letter && string(password[max -1]) != letter) || 
+		(max <= len(password) && string(password[min -1]) != letter && string(password[max -1]) == letter){
+			partTwoValid++
+		}
+	}
+	fmt.Printf("PartOne valid= %d\n", valid)
+	fmt.Printf("PartTwo valid= %d\n", partTwoValid)
+
+}
